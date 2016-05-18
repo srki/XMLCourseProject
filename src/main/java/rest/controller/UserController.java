6adb8822b21cd189fa519e4a0fa17eadf4d9f371
vladimirivkovic <vlado.ivkovic@live.com>
2016-05-18 23:56:54 +0200

    media type changed for users rest

--- src/main/java/rest/controller/UserController.java
+++ src/main/java/rest/controller/UserController.java
@@ -8,6 +8,8 @@ import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
@@ -17,10 +19,11 @@ public class UserController {
     IUserDao userDao;
 
     @GET
+    @Produces(MediaType.APPLICATION_XML)
     public Object getUsers() {
         try {
             Users users = userDao.getUsers();
-            return Response.status(200).entity(users.toString()).build();
+            return Response.status(200).entity(users).build();
         } catch (Exception e) {
             return Response.status(400).build();
         }
@@ -28,10 +31,11 @@ public class UserController {
 
     @GET
     @Path("/{username}")
+    @Produces(MediaType.APPLICATION_XML)
     public Object getUser(@PathParam("username") String username) {
         try {
             User u = userDao.getUser(username);
-            return Response.status(200).entity(u.toString()).build();
+            return Response.status(200).entity(u).build();
         } catch (Exception e) {
             return Response.status(400).build();
         }
