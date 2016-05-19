6a3c2d02d7e9546cf8024cd5f54ce9582af05a09
Srđan Milaković <srki@outlook.com>
2016-05-19 15:14:25 +0200

    Added directive that wraps XML editor.

---
+++ src/main/webapp/xml-editor/editor.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Srđan on 19.5.2016..
+ */
+
+$(function () {
+    var keyValueList = window.location.search.substring(1).split('&'), params = {};
+    for (var i = 0 in keyValueList) {
+        if (keyValueList.hasOwnProperty(i)) {
+            var keyValue = keyValueList[i].split('='); //split key and value
+            params[keyValue[0]] = keyValue[1];
+        }
+    }
+
+    console.log(params);
+
+    var extractor = new Xsd2Json(params.schemaName, {
+            schemaURI: params.schemaUri,
+            rootElement: params.rootElement
+        }),
+        elementText = '<element xlmns="http://ftn.uns.ac.rs/xml"></element>'
+            .split('element')
+            .join(params.rootElement);
+
+    $('#XMLEditor')
+        .text(elementText)
+        .xmlEditor({
+            menuEntries: [],
+            enforceOccurs: true,
+            prependNewElements: true,
+            schema: extractor.getSchema(),
+            ajaxOptions: {
+                xmlUploadPath: params.submitPath
+            }
+        });
+});
