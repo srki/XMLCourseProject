c366bb5d31003ea1c8441c23994f57aae40b4463
aleksandar <93lukic@live.com>
2016-05-22 20:34:02 +0200

    Updated act filter view and controller

--- src/main/webapp/app/controllers/ActFilterCtrl.js
+++ src/main/webapp/app/controllers/ActFilterCtrl.js
@@ -7,18 +7,28 @@
 
     angular.module('app.ActFilterCtrl', [])
 
-        .controller('ActFilterCtrl', ['$scope', 'Acts',
+        .controller('ActFilterCtrl', ['$scope', 'Acts', '$location',
 
-            function ($scope, Acts) {
+            function ($scope, Acts, $location) {
 
                 $scope.data = [];
+                $scope.state = 'idle';
 
                 $scope.get = function () {
 
-                    Acts.get($scope.filter).then(
+                    var filter = JSON.parse(JSON.stringify($scope.filter));
+
+                    if(filter.start_date)
+                        filter.start_date = $scope.filter.start_date.getTime();
+
+                    if(filter.end_date)
+                        filter.end_date = $scope.filter.end_date.getTime();
+
+                    Acts.get(filter).then(
 
                         function (response) {
-                            $scope.data = response.data.acts || [];
+                            $scope.data = response.data.acts.act || [];
+                            $scope.state = ($scope.data.length > 0)? 'showResults':'noResults';
                             console.log(response);
                         },
 
@@ -33,8 +43,11 @@
                     $scope.filter = {};
                 };
 
-                $scope.clear();
+                $scope.update = function (index) {
+                    $location.path('/acts/' + $scope.data[index].uri);
+                };
 
+                $scope.clear();
             }
         ]);
 
