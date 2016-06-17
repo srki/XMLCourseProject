bfc96086022073c15de722a0100a0236f0aba1ca
Srđan Milaković <srki@outlook.com>
2016-06-17 19:14:43 +0200

    Added article hash links.

--- src/main/webapp/app/controllers/EditActCtrl.js
+++ src/main/webapp/app/controllers/EditActCtrl.js
@@ -6,26 +6,26 @@
     "use strict";
 
     angular.module('app.EditActCtrl', [])
-        .controller('EditActCtrl', function ($scope, $sce, $routeParams, $location, Auth, Acts) {
+        .controller('EditActCtrl', function ($scope, $sce, $routeParams, $location, $anchorScroll, Auth, Acts) {
             Auth.isLogged(function (isLogged) {
                 if (!isLogged) {
                     $location.path('/login');
                 }
             });
 
+            $scope.scroll = function (id) {
+                if (id || id == 0) {
+                    $location.hash(id);
+                }
+                $anchorScroll();
+            };
+
             (function () {
                 if ($routeParams.edit) {
-                    Acts.getHtmlEdit($routeParams.id).then(
-                        function (response) {
-                            $scope.actHtml = $sce.trustAsHtml(response.data);
-                        });
+                    $scope.url = 'api/export/html-edit/' + $routeParams.id;
                 } else {
-                    Acts.getHtml($routeParams.id).then(
-                        function (response) {
-                            $scope.actHtml = $sce.trustAsHtml(response.data);
-                        });
+                    $scope.url = 'api/export/html/' + $routeParams.id;
                 }
-
             }());
         });
 }(angular));
